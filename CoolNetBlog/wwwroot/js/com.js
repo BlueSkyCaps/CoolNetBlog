d61cd761cb03a834f499eb213a530b380908afc5
lsx-first <1572491001@qq.com>
2022-03-16 15:17:52 +0800

    collapsed

--- CoolNetBlog/wwwroot/js/com.js
+++ CoolNetBlog/wwwroot/js/com.js
@@ -13,3 +13,10 @@
         + (seconds > 9 ? seconds : "0" + seconds);
     return commnetTimeStr;
 }
+
+String.prototype.format = function () {
+    if (arguments.length == 0) return this;
+    for (var s = this, i = 0; i < arguments.length; i++)
+        s = s.replace(new RegExp("\\{" + i + "\\}", "g"), arguments[i]);
+    return s;
+};
