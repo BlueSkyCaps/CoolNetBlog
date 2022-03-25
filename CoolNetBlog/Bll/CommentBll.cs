46d29f41ebb4582482626ceb371342c893dac1f8
lsx-first <1572491001@qq.com>
2022-03-25 14:17:19 +0800

    _

--- CoolNetBlog/Bll/CommentBll.cs
+++ CoolNetBlog/Bll/CommentBll.cs
@@ -12,24 +12,29 @@ namespace CoolNetBlog.Bll
         private readonly BaseSugar _baseSugar;
         private readonly SugarDataBaseStorage<Article, int> _articleSet;
         private readonly SugarDataBaseStorage<Comment, int> _commentSet;
+        private readonly SugarDataBaseStorage<Reply, int> _replySet;
+        private ValueResult _result;
+        
         public CommentBll()
         {
+            _result = new()
+            {
+                Code = ValueCodes.UnKnow
+            };
             _baseSugar = new BaseSugar();
             _articleSet = new SugarDataBaseStorage<Article, int>(_baseSugar._dbHandler);
             _commentSet = new SugarDataBaseStorage<Comment, int>(_baseSugar._dbHandler);
+            _replySet = new SugarDataBaseStorage<Reply, int>(_baseSugar._dbHandler);
+
         }
 
         public async Task<ValueResult> DealCommentPostAsync(CommentViewModel data, HttpContext httpContext) { 
-            ValueResult result = new()
-            { 
-                 Code = ValueCodes.UnKnow
-            };
             if (data.SourceType<=0||data.SourceId<=0)
             {
-                result.Code = ValueCodes.Error;
-                result.HideMessage = "评论SourceType|SourceId不是有效值";
-                result.TipMessage = "发表评论失败了，可以刷新再试试哦..";
-                return result;
+                _result.Code = ValueCodes.Error;
+                _result.HideMessage = "评论SourceType|SourceId不是有效值";
+                _result.TipMessage = "发表评论失败了，可以刷新再试试哦..";
+                return _result;
             }
             Article? commentAte = null;
             // SourceType == 1 文章评论。
@@ -39,50 +44,59 @@ namespace CoolNetBlog.Bll
                 if (string.IsNullOrWhiteSpace(data.Name)|| string.IsNullOrWhiteSpace(data.Email) || 
                     string.IsNullOrWhiteSpace(data.Content))
                 {
-                    result.HideMessage = "没有填写完整";
-                    result.TipMessage = "请输入必要内容哦..";
-                    return result;
+                    _result.HideMessage = "没有填写完整";
+                    _result.TipMessage = "请输入必要内容哦..";
+                    return _result;
                 }
 
                 if (data.Name.Length>12 || data.Email.Length>25 ||  data.Content.Length>150)
                 {
-                    result.HideMessage = "长度超出";
-                    result.TipMessage = "字数超出了规定长度..";
-                    return result;
+                    _result.HideMessage = "长度超出";
+                    _result.TipMessage = "字数超出了规定长度..";
+                    return _result;
                 }
                 if (!string.IsNullOrWhiteSpace(data.SiteUrl))
                 {
                     bool passUri = PathProvider.TryCreateUrl(data.SiteUrl);
                     if (!passUri)
                     {
-                        result.HideMessage = "输入了网址uri但无效";
-                        result.TipMessage = "不是合格的网址哦..";
-                        return result;
+                        _result.HideMessage = "输入了网址uri但无效";
+                        _result.TipMessage = "不是合格的网址哦..";
+                        return _result;
                     }
                 }
 
                 commentAte = await _articleSet.FindOneByIdAsync(data.SourceId);
                 if (commentAte is null)
                 {
-                    result.HideMessage = "不存在此文章Id";
-                    result.TipMessage = "该文章已经消失了，可以刷新看看哦..";
-                    return result;
+                    _result.HideMessage = "不存在此文章Id";
+                    _result.TipMessage = "该文章已经消失了，可以刷新看看哦..";
+                    return _result;
                 }
                 if (commentAte.CommentType == 3)
                 {
-                    result.Code = ValueCodes.UnKnow;
-                    result.HideMessage = "不允许评论的内容尝试进行评论";
-                    result.TipMessage = "内容已被设置为不允许评论~";
-                    return result;
+                    _result.Code = ValueCodes.UnKnow;
+                    _result.HideMessage = "不允许评论的内容尝试进行评论";
+                    _result.TipMessage = "内容已被设置为不允许评论~";
+                    return _result;
                 }
             }
             var cip = httpContext.Connection.RemoteIpAddress?.ToString()??"";
             if (string.IsNullOrWhiteSpace(cip))
             {
-                result.HideMessage = "新增评论,获取不到客户端ip";
-                result.TipMessage = "评论失败了呢，可以重新试试。";
-                return result;
+                _result.HideMessage = "新增评论,获取不到客户端ip";
+                _result.TipMessage = "评论失败了呢，可以重新试试。";
+                return _result;
             }
+            
+            if (!LeaveMessageLimitViaAsync(cip))
+            {
+                _result.HideMessage = "新增评论,此ip超过当日限制次数";
+                _result.TipMessage = "你当日发言次数超过限制，第二天再来哦~";
+                return _result;
+            }
+
+            
             Comment insertComment = new Comment
             {
                 CommentTime = DateTime.Now,
@@ -97,10 +111,10 @@ namespace CoolNetBlog.Bll
 
             if (await _baseSugar._dbHandler.Queryable<AdminUser>().AnyAsync(a => a.AccountName.ToLower() == insertComment.Name.ToLower()))
             {
-                result.Code = ValueCodes.UnKnow;
-                result.HideMessage = "昵称是管理员使用昵称";
-                result.TipMessage = "哈,该昵称被创造者'霸占'了哦~";
-                return result;
+                _result.Code = ValueCodes.UnKnow;
+                _result.HideMessage = "昵称是管理员使用昵称";
+                _result.TipMessage = "哈,该昵称被创造者'霸占'了哦~";
+                return _result;
             }
 
             // SourceType == 1 文章评论
@@ -118,29 +132,24 @@ namespace CoolNetBlog.Bll
             catch (Exception e)
             {
                 _commentSet.TransRoll();
-                result.Code = ValueCodes.Error;
-                result.HideMessage = "插入评论数据时引发异常:"+e.Message+" "+e.StackTrace;
-                result.TipMessage = "评论发表失败了，你可以再试一下?!";
+                _result.Code = ValueCodes.Error;
+                _result.HideMessage = "插入评论数据时引发异常:"+e.Message+" "+e.StackTrace;
+                _result.TipMessage = "评论发表失败了，你可以再试一下?!";
             }
-            result.Code = ValueCodes.Success;
-            result.TipMessage = commentAte?.CommentType == 2? "经过审核后将会显示评论,感谢您的畅所欲言~" : "感谢您的畅所欲言~";
-            return result;
+            _result.Code = ValueCodes.Success;
+            _result.TipMessage = commentAte?.CommentType == 2? "经过审核后将会显示评论,感谢您的畅所欲言~" : "感谢您的畅所欲言~";
+            return _result;
         }
 
         public async Task<ValueResult> DealReplyPostAsync(ReplyViewModel data, HttpContext httpContext)
         {
-            SugarDataBaseStorage<Reply, int> replySet = new SugarDataBaseStorage<Reply, int>(_baseSugar._dbHandler);
 
-            ValueResult result = new()
-            {
-                Code = ValueCodes.UnKnow
-            };
             if (data.CommentId <= 0 )
             {
-                result.Code = ValueCodes.Error;
-                result.HideMessage = "回复CommentId不是有效值";
-                result.TipMessage = "回复失败了，可以刷新再试试哦..";
-                return result;
+                _result.Code = ValueCodes.Error;
+                _result.HideMessage = "回复CommentId不是有效值";
+                _result.TipMessage = "回复失败了，可以刷新再试试哦..";
+                return _result;
             }
             Article? commentAte = null;
             Comment? cmt = null;
@@ -148,51 +157,59 @@ namespace CoolNetBlog.Bll
             if (string.IsNullOrWhiteSpace(data.Name) || string.IsNullOrWhiteSpace(data.Email) ||
                 string.IsNullOrWhiteSpace(data.Content))
             {
-                result.HideMessage = "没有填写完整";
-                result.TipMessage = "请输入必要内容哦..";
-                return result;
+                _result.HideMessage = "没有填写完整";
+                _result.TipMessage = "请输入必要内容哦..";
+                return _result;
             }
 
             if (data.Name.Length > 12 || data.Email.Length > 25 || data.Content.Length > 150)
             {
-                result.HideMessage = "长度超出";
-                result.TipMessage = "字数超出了规定长度..";
-                return result;
+                _result.HideMessage = "长度超出";
+                _result.TipMessage = "字数超出了规定长度..";
+                return _result;
             }
             if (!string.IsNullOrWhiteSpace(data.SiteUrl))
             {
                 bool passUri = PathProvider.TryCreateUrl(data.SiteUrl);
                 if (!passUri)
                 {
-                    result.HideMessage = "输入了网址uri但无效";
-                    result.TipMessage = "不是合格的网址哦..";
-                    return result;
+                    _result.HideMessage = "输入了网址uri但无效";
+                    _result.TipMessage = "不是合格的网址哦..";
+                    return _result;
                 }
             }
             // 获取此回复对应评论实体
             cmt = await _commentSet.FindOneByIdAsync(data.CommentId);
             if (cmt is null)
             {
-                result.HideMessage = "不存在此评论Id";
-                result.TipMessage = "评论已经消失了，可以刷新看看哦..";
-                return result;
+                _result.HideMessage = "不存在此评论Id";
+                _result.TipMessage = "评论已经消失了，可以刷新看看哦..";
+                return _result;
             }
             // 通过评论实体获取对应的文章实体
             commentAte = await _articleSet.FindOneByIdAsync(cmt.SourceId);
             if (commentAte.CommentType == 3)
             {
-                result.Code = ValueCodes.UnKnow;
-                result.HideMessage = "不允许评论的内容尝试进行评论";
-                result.TipMessage = "内容已被设置为不允许评论~";
-                return result;
+                _result.Code = ValueCodes.UnKnow;
+                _result.HideMessage = "不允许评论的内容尝试进行评论";
+                _result.TipMessage = "内容已被设置为不允许评论~";
+                return _result;
             }
             var cip = httpContext.Connection.RemoteIpAddress?.ToString() ?? "";
             if (string.IsNullOrWhiteSpace(cip))
             {
-                result.HideMessage = "新增回复,获取不到客户端ip";
-                result.TipMessage = "回复失败了呢，可以重新试试。";
-                return result;
+                _result.HideMessage = "新增回复,获取不到客户端ip";
+                _result.TipMessage = "回复失败了呢，可以重新试试。";
+                return _result;
             }
+
+            if (!LeaveMessageLimitViaAsync(cip))
+            {
+                _result.HideMessage = "新增评论,此ip超过当日限制次数";
+                _result.TipMessage = "你当日发言次数超过限制，第二天再来哦~";
+                return _result;
+            }
+
             Reply insertReply = new Reply
             {
                 ReplyTime = DateTime.Now,
@@ -206,10 +223,10 @@ namespace CoolNetBlog.Bll
             };
             if (await _baseSugar._dbHandler.Queryable<AdminUser>().AnyAsync(a => a.AccountName.ToLower() == insertReply.Name.ToLower()))
             {
-                result.Code = ValueCodes.UnKnow;
-                result.HideMessage = "昵称是管理员使用昵称";
-                result.TipMessage = "哈,该昵称被创造者'霸占'了哦~";
-                return result;
+                _result.Code = ValueCodes.UnKnow;
+                _result.HideMessage = "昵称是管理员使用昵称";
+                _result.TipMessage = "哈,该昵称被创造者'霸占'了哦~";
+                return _result;
             }
 
             if (cmt.SourceType == 1)
@@ -217,22 +234,22 @@ namespace CoolNetBlog.Bll
                 // 要回复的评论的文章若是设置需要审核(CommentType为2) IsPassed就是false，否则公开评论直接true
                 insertReply.IsPassed = commentAte?.CommentType == 2 ? false : true;
             }
-            replySet.TransBegin();
+            _replySet.TransBegin();
             try
             {
-                var e = await replySet.InsertAsync(insertReply);
-                replySet.TransCommit();
+                var e = await _replySet.InsertAsync(insertReply);
+                _replySet.TransCommit();
             }
             catch (Exception e)
             {
-                replySet.TransRoll();
-                result.Code = ValueCodes.Error;
-                result.HideMessage = "插入回复数据时引发异常:" + e.Message + " " + e.StackTrace;
-                result.TipMessage = "回复失败了，可以再试一下?!";
+                _replySet.TransRoll();
+                _result.Code = ValueCodes.Error;
+                _result.HideMessage = "插入回复数据时引发异常:" + e.Message + " " + e.StackTrace;
+                _result.TipMessage = "回复失败了，可以再试一下?!";
             }
-            result.Code = ValueCodes.Success;
-            result.TipMessage = commentAte?.CommentType == 2 ? "审核后将显示,感谢发言~" : "感谢畅所欲言~";
-            return result;
+            _result.Code = ValueCodes.Success;
+            _result.TipMessage = commentAte?.CommentType == 2 ? "审核后将显示,感谢发言~" : "感谢畅所欲言~";
+            return _result;
         }
 
         public async Task<ValueResult> GetArticleCommentsAsync(int sourceId, int index)
@@ -240,17 +257,13 @@ namespace CoolNetBlog.Bll
             SugarDataBaseStorage<CommentViewModel, int> commentVmReader= new SugarDataBaseStorage<CommentViewModel, int>(_baseSugar._dbHandler);
             SugarDataBaseStorage<ReplyViewModel, int> replyVmReader = new SugarDataBaseStorage<ReplyViewModel, int>(_baseSugar._dbHandler);
 
-            ValueResult result = new()
-            {
-                Code = ValueCodes.UnKnow
-            };
             Article? commentAte = null;
             commentAte = await _articleSet.FindOneByIdAsync(sourceId);
             if (commentAte is null)
             {
-                result.HideMessage = $"获取文章id为{sourceId}的评论时失败，不存在此文章Id";
-                result.TipMessage = "加载评论失败了，刷新在试试吧?!";
-                return result;
+                _result.HideMessage = $"获取文章id为{sourceId}的评论时失败，不存在此文章Id";
+                _result.TipMessage = "加载评论失败了，刷新在试试吧?!";
+                return _result;
             }
             try
             {
@@ -268,33 +281,29 @@ namespace CoolNetBlog.Bll
                     // 计算是否不止5个
                     c.HasReplyInNext = (await queryable.CountAsync())>5;
                 }
-                result.Data = commentsVm;
-                result.Code = ValueCodes.Success;
+                _result.Data = commentsVm;
+                _result.Code = ValueCodes.Success;
             }
             catch (Exception e)
             {
-                result.Code = ValueCodes.Error;
-                result.HideMessage = $"获取文章id为{sourceId}的评论时失败，引发异常:{e.Message} {e.StackTrace}";
-                result.TipMessage = "加载评论失败了，刷新在试试吧?!";
+                _result.Code = ValueCodes.Error;
+                _result.HideMessage = $"获取文章id为{sourceId}的评论时失败，引发异常:{e.Message} {e.StackTrace}";
+                _result.TipMessage = "加载评论失败了，刷新在试试吧?!";
             }
-            return result;
+            return _result;
         }
 
         public async Task<ValueResult> GetCommentReplysAsync(int commentId, int index)
         {
             SugarDataBaseStorage<ReplyViewModel, int> replyVmReader = new SugarDataBaseStorage<ReplyViewModel, int>(_baseSugar._dbHandler);
 
-            ValueResult result = new()
-            {
-                Code = ValueCodes.UnKnow
-            };
             Comment? comment = null;
             comment = await _commentSet.FindOneByIdAsync(commentId);
             if (comment is null)
             {
-                result.HideMessage = $"获取评论id为{commentId}的评论失败，不存在此评论Id";
-                result.TipMessage = "评论已经消失了，可以刷新看看哦..";
-                return result;
+                _result.HideMessage = $"获取评论id为{commentId}的评论失败，不存在此评论Id";
+                _result.TipMessage = "评论已经消失了，可以刷新看看哦..";
+                return _result;
             }
             try
             {
@@ -304,16 +313,45 @@ namespace CoolNetBlog.Bll
                 var replies = await queryable.Skip((index - 1) * 5).Take(5).ToListAsync();
                 // 计算是否总数大于当前页*条数
                 var HasReplyInNext = (await queryable.CountAsync()) > index * 5;
-                result.Data = new { Replies = replies, HasReplyInNext };
-                result.Code = ValueCodes.Success;
+                _result.Data = new { Replies = replies, HasReplyInNext };
+                _result.Code = ValueCodes.Success;
             }
             catch (Exception e)
             {
-                result.Code = ValueCodes.Error;
-                result.HideMessage = $"获取评论id为{commentId}的评论时失败，引发异常:{e.Message} {e.StackTrace}";
-                result.TipMessage = "获取回复失败了，刷新在试试吧?!";
+                _result.Code = ValueCodes.Error;
+                _result.HideMessage = $"获取评论id为{commentId}的评论时失败，引发异常:{e.Message} {e.StackTrace}";
+                _result.TipMessage = "获取回复失败了，刷新在试试吧?!";
             }
-            return result;
+            return _result;
         }
+
+
+        /// <summary>
+        /// 当前评论回复的ip是否已超过当日留言限制次数
+        /// </summary>
+        /// <returns></returns>
+        private async Task<bool> LeaveMessageLimitViaAsync(string cip)
+        {
+            SugarDataBaseStorage<SiteSetting, int> sittSet = new SugarDataBaseStorage<SiteSetting, int>(_baseSugar._dbHandler);
+            var theSitt = await sittSet.FirstOrDefaultAsync(s => 1 == 1);
+            if (theSitt is null)
+            {
+                _result.Code = ValueCodes.Error;
+                _result.HideMessage = "新增评论,获取不到SiteSetting表";
+                _result.TipMessage = "评论失败了呢，可以重新试试。";
+                return false;
+            }
+            int? lmt = (await sittSet.FirstOrDefaultAsync(s => 1 == 1))?.LeaveLimitCount;
+            if (!(lmt == null || lmt <= 0))
+            {
+                _commentSet.GetListBuilder().Where(c=>c.ClientIp==cip).Where(c=>c.CommentTime)
+                _result.HideMessage = "新增评论,此ip超过当日限制次数";
+                _result.TipMessage = "你当日发言次数超过限制，第二天再来哦~";
+                return false;
+            }
+            return true;
+        }
+
+           
     }
 }
