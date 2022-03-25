6a330a2be7cab9ec3be99bfc52a1954c290b6672
lsx-first <1572491001@qq.com>
2022-03-25 11:06:12 +0800

    searchInputEnterDown输入框监听键盘

---
+++ CoolNetBlog/wwwroot/js/view/Shared/_Layout.js
@@ -0,0 +1,20 @@
+﻿$('.toSearchBtn').click(function () {
+    // 若搜索框输入值不是空白 get跳转搜索
+    var kwValue = $('#' + event.target.id).parent('form').children(':input[name="kw"]').val();
+    if (kwValue == undefined || kwValue == 'undefined' || $.trim(kwValue) == '') {
+        return;
+    }
+    var getSearchUri = '/Home/Index/?kw=' + $.trim(kwValue) + '&from=keyword';
+    $(location).attr('href', getSearchUri);
+});
+
+function searchInputEnterDown(target) {
+    if (event.key == 'Enter') {
+        var kwValue = $(target).val();
+        if (kwValue == undefined || kwValue == 'undefined' || $.trim(kwValue) == '') {
+            return;
+        }
+        var getSearchUri = '/Home/Index/?kw=' + $.trim(kwValue) + '&from=keyword';
+        $(location).attr('href', getSearchUri);
+    }
+}
