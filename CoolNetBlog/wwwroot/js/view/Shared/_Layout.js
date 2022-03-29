c4f88fa7603c60563fbd0b529661dc90a649e9b1
lsx-first <1572491001@qq.com>
2022-03-29 09:26:25 +0800

    固定顶部导航栏在顶部位置 当页面滚动时

--- CoolNetBlog/wwwroot/js/view/Shared/_Layout.js
+++ CoolNetBlog/wwwroot/js/view/Shared/_Layout.js
@@ -18,3 +18,21 @@ function searchInputEnterDown(target) {
         $(location).attr('href', getSearchUri);
     }
 }
+
+let scrollPos= 0;
+let ticking = false;
+
+// 固定顶部导航栏在顶部位置 当页面滚动时
+function navFixedHandler(scrollPos) {
+    if (scrollPos > 100) {
+        $('#topNav').addClass('fixed-top');
+    } else {
+        $('#topNav').removeClass('fixed-top');
+    }
+}
+
+// 当页面滚动时 触发事件
+window.addEventListener('scroll', function (e) {
+    scrollPos = window.scrollY;
+    navFixedHandler(scrollPos);
+});
