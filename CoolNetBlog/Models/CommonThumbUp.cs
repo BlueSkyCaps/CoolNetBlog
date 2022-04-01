11f3218753df1d0f5c0ca51e73018de396bc4d29
lsx-first <1572491001@qq.com>
2022-04-01 17:18:20 +0800

    add CommonThumbUp and gossop table

---
+++ CoolNetBlog/Models/CommonThumbUp.cs
@@ -0,0 +1,34 @@
+﻿using SqlSugar;
+
+namespace CoolNetBlog.Models
+{
+    /// <summary>
+    /// 统一点赞表
+    /// </summary>
+    [SugarTable("CommonThumbUp")]
+    public class CommonThumbUp
+    {
+        /// <summary>
+        /// Default:源Id
+        /// Nullable:False
+        /// </summary>           
+        [SugarColumn(IsPrimaryKey = true)]
+        public int SourceId { get; set; }
+        /// <summary>
+        /// Default:源Id所属类型：1"闲言碎语"组件表，2.. (文章点赞不使用此表而是专门的文章点赞表ArticleThumbUp)
+        /// Nullable:False
+        /// </summary>           
+        [SugarColumn(IsPrimaryKey = true)]
+        public int SourceType { get; set; }
+
+        /// <summary>
+        /// Desc:客户端点赞文章的Ip
+        /// Default:
+        /// Nullable:False
+        /// </summary>           
+        public string ClientIp { get; set; }
+        public string? ClientDevice { get; set; }
+        public string? ClientBrowser { get; set; }
+        public DateTime? UpTime { get; set; }
+    }
+}
---
+++ CoolNetBlog/Models/Gossip.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SqlSugar;
+
+namespace CoolNetBlog.Models
+{
+    ///<summary>
+    /// "闲言碎语"组件表
+    ///</summary>
+    [SugarTable("Gossip")]
+    public partial class Gossip
+    {
+        public Gossip(){
+
+
+        }
+        /// <summary>
+        /// Desc:自增主键
+        /// Default:
+        /// Nullable:False
+        /// </summary>           
+        [SugarColumn(IsPrimaryKey=true,IsIdentity=true)]
+        public int Id {get;set;}
+
+        /// <summary>
+        /// Desc:内容
+        /// Default:
+        /// Nullable:False
+        /// </summary>           
+        public string Content {get;set;}
+
+        public DateTime AddTime {get;set;}
+
+        public int StarNumber { get; set; }
+
+    }
+}
