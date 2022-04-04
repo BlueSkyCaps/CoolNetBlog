bb7bfa51218d7f8c7a60505bee4dd165287d01ec
lsx-first <1572491001@qq.com>
2022-04-04 15:17:34 +0800

    WebEncoderOptions解决unicode中文编码显示

--- CoolNetBlog/Program.cs
+++ CoolNetBlog/Program.cs
@@ -1,18 +1,26 @@
 using CoolNetBlog.Base;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.WebEncoders;
 using System.Text.Encodings.Web;
+using System.Text.Unicode;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
+builder.Services.Configure<WebEncoderOptions>(options =>
+{
+    options.TextEncoderSettings = new TextEncoderSettings(UnicodeRanges.All);
+});
+
 builder.Services.AddControllersWithViews().AddJsonOptions((options =>
 {
     // 配置System.Text.Json 全局Json非英文字符
     //options.JsonSerializerOptions.Encoder = JavaScriptEncoder.Create(UnicodeRanges.All);//此配置部分字符无法原封显示
     options.JsonSerializerOptions.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
 }));
+
 
 builder.Services.AddMvc(options =>
 {
