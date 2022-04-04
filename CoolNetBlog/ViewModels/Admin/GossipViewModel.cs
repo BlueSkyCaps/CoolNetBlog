9ad331640736784e87f54386875d91d85f146cfd
lsx-first <1572491001@qq.com>
2022-04-04 15:01:56 +0800

    <meta name="description"> 使用@Html.Raw避免html编码

---
+++ CoolNetBlog/ViewModels/Admin/GossipViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace CoolNetBlog.ViewModels.Admin
+{
+    public class GossipViewModel:PassBaseViewModel
+    {
+        public int Id { get; set; }
+
+        public int Type { get; set; }
+
+        public string? ImgUrl { get; set; }
+        public string Content { get; set; }
+
+        public DateTime AddTime { get; set; }
+
+        public int StarNumber { get; set; }
+    }
+}
